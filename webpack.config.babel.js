9c3c30fa474f902fa9e799de3f7e78669cadb003
roeyherzel <roeyherzel@gmail.com>
2018-06-04 16:56:17 -0700

    Environment setup
    - yarn
    - webpack/dev-server
    - babel
    - react
    - hmr
    - hello world

---
+++ webpack.config.babel.js
@@ -0,0 +1,45 @@
+import path from 'path';
+import webpack from 'webpack';
+import CleanWebpackPlugin from 'clean-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+
+export default {
+    entry: './src/index.js',
+
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+        overlay: true,
+        stats: 'minimal'
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Weather App',
+            template: './src/index.html',
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+};
