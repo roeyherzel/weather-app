089ef9a3927d6d0bdd2a0bdc79115b525885cff1
roeyherzel <roeyherzel@gmail.com>
2018-06-04 17:20:56 -0700

    setup styled components + stylelint

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
 import { hot } from 'react-hot-loader'
 
 
-const App = () => <h1>Hello, world!</h1>;
+const HelloWorld = styled.h1`
+    color: red;
+`
+
+const App = () => <HelloWorld>Hello, world!</HelloWorld>;
 
 export default hot(module)(App);
