02f70773bd5b4c568244039ce6a425b20b64a38c
roeyherzel <roeyherzel@gmail.com>
2018-06-15 23:48:17 -0700

    condition component

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Segment, Header, Statistic } from 'semantic-ui-react';
+import { Segment, Header, Statistic, Icon } from 'semantic-ui-react';
 
 import WeatherIcon from './WeathIcon';
 
 
 const Container = styled.div`
     margin: 6rem;
+`;
+
+
+const Condition = styled.div`
+    display: flex;
+    align-items: center;
+    margin-top: 1rem;
+
+    & > .wi {
+        font-size: 3rem;
+        margin-right: 1rem;
+    }
+
+    & > .ui.header:last-child {
+        margin: 0;
+    }
 `;
 
 const Location = ({
@@ -25,26 +41,26 @@ const Location = ({
     <Container>
         <Segment>
             <Header>{ city }, { country }</Header>
-            <Statistic.Group size="large">
-                <Statistic>
-                    <Statistic.Value>
-                        { temp }&deg;
-                    </Statistic.Value>
-                </Statistic>
+            <Statistic>
+                <Statistic.Value>
+                    { temp }
+                    <WeatherIcon name="degrees" />
+                </Statistic.Value>
+            </Statistic>
+            <Condition>
+                <WeatherIcon id={weatherId} icon={icon} />
+                <Header size="large" content={main} subheader={description} />
+            </Condition>
 
-                <Statistic>
-                    <Statistic.Value>
-                        <WeatherIcon id={weatherId} icon={icon} />
-                    </Statistic.Value>
-                    <Statistic.Label>{ main }</Statistic.Label>
-                </Statistic>
-            </Statistic.Group>
 
             <Statistic.Group size="small">
                 <Statistic value={humidity} label="humidity" />
                 <Statistic value={wind.speed} label="wind" />
             </Statistic.Group>
+
         </Segment>
+
+
     </Container>
 );
 
